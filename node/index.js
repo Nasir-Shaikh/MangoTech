74a1431bfa28a97ef90b138d076e0fbba07dfeed
Nasir Shaik <91408537+Nasir-Shaikh@users.noreply.github.com>
2023-11-15 18:35:49 +0530

    Create index.js

---
+++ node/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// ... (the rest of your existing code)
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
